c3f58babbf7cbfd405f6d456cec8a6abe886954b
Neville Brem <contact@nevillebrem.com>
2023-12-22 07:10:04 +0100

    feat: navbar finished & home page initialized

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import { AppShell, RadioGroup, RadioItem, storeHighlightJs } from '@skeletonlabs/skeleton';
 	import xml from 'highlight.js/lib/languages/xml'; // for HTML
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
@@ -19,7 +19,46 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	let value: number = 0;
+	$: if ($page.url.pathname === '/') {
+		value = 0;
+	} else if ($page.url.pathname === '/work') {
+		value = 1;
+	} else if ($page.url.pathname === '/contact') {
+		value = 2;
+	}
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+		<RadioGroup class="fixed left-6 top-1/2 -translate-y-1/2" flexDirection="flex-col" gap="gap-2">
+			<RadioItem
+				bind:group={value}
+				name="home"
+				value={0}
+				class="rounded-full aspect-square flex justify-center text-xl"
+				><a class="m-auto flex gap-1 group" href="/"
+					><i class="fa-solid fa-house"></i>
+				</a></RadioItem
+			>
+			<RadioItem
+				bind:group={value}
+				name="work"
+				value={1}
+				class="rounded-full aspect-square flex justify-center text-xl"
+				><a class="m-auto" href="/work"><i class="fa-solid fa-suitcase"></i></a></RadioItem
+			>
+			<RadioItem
+				bind:group={value}
+				name="contact"
+				value={2}
+				class="rounded-full aspect-square flex justify-center text-xl"
+				><a class="m-auto" href="/contact"><i class="fa-solid fa-address-book"></i></a></RadioItem
+			>
+		</RadioGroup>
+	</svelte:fragment>
+	<slot />
+</AppShell>
