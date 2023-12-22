c822b0bc10ac1c010d249006590eff84ecb8f972
Neville Brem <contact@nevillebrem.com>
2023-12-22 18:43:35 +0100

    style: added 3d

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	let name = 'Neville Brem';
+	import { Application } from '@splinetool/runtime';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const canvas = document.getElementById('canvas3d');
+		const app = new Application(canvas);
+		app.load('https://prod.spline.design/D7V0YF6owfNH8OS2/scene.splinecode');
+	});
 </script>
 
 <div class="flex w-screen h-screen justify-center items-center">
@@ -10,10 +18,12 @@
 			{/each}
 		</p>
 		<h3 class="mx-auto italic h3 pt-4">
-			Bringing <span
-				class="before:block before:absolute before:-inset-1 before:skew-y-3 before:bg-secondary-500 relative inline-block"
-				><span class="relative">life</span></span
-			> to every page
+			Bringing <span class="font-black text-primary-500">life</span> to every page
 		</h3>
+		<button
+			class="font-bold btn bg-primary-500 mx-auto mt-6 text-xl p-4 border-b-4 border-primary-200 hover:border-b-8 active:border-none rounded-3xl"
+			>Explore</button
+		>
 	</div>
+	<canvas id="canvas3d" class="fixed -z-10"></canvas>
 </div>
