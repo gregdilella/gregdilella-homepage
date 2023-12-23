87bc2c40a79db30962ba7aeac23e8a8adca66aa6
Neville Brem <contact@nevillebrem.com>
2023-12-23 10:25:12 +0100

    feat: added about section

---
+++ src/lib/macintosh.svelte
@@ -0,0 +1,6 @@
+<script
+	type="module"
+	src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"
+></script>
+
+<spline-viewer url="https://prod.spline.design/H-wZEaPskiwj5NSS/scene.splinecode"></spline-viewer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { popup, type PopupSettings } from '@skeletonlabs/skeleton';
 
+	import { Application } from '@splinetool/runtime';
+	import { onMount } from 'svelte';
+
 	let name = 'Neville Brem';
 
 	const popupExplore: PopupSettings = {
@@ -8,6 +11,16 @@
 		target: 'popupExplore',
 		placement: 'bottom'
 	};
+	const popupMac: PopupSettings = {
+		event: 'hover',
+		target: 'popupMac',
+		placement: 'bottom'
+	};
+	onMount(() => {
+		const canvas = document.getElementById('canvas3d');
+		const app = new Application(canvas);
+		app.load('https://prod.spline.design/H-wZEaPskiwj5NSS/scene.splinecode');
+	});
 </script>
 
 <div class="flex w-screen h-screen justify-center items-center">
@@ -25,10 +38,27 @@
 		<h3 class="mx-auto italic h3 pt-4">
 			Bringing <span class="font-black text-primary-500">life</span> to every page
 		</h3>
-		<button
-			class="font-bold btn bg-primary-500 mx-auto mt-6 text-xl p-4 border-b-8 hover:border-b-0 border-primary-200 hover:border-t-8 active:border-none rounded-3xl"
-			use:popup={popupExplore}>Explore</button
+		<a
+			href="#about"
+			class="font-bold btn bg-primary-500 mx-auto mt-6 text-xl p-4 border-b-4 border-primary-200 hover:border-b-8 active:border-none rounded-3xl"
+			use:popup={popupExplore}>Explore</a
 		>
 		<p data-popup="popupExplore" class="text-sm font-semibold">curious?</p>
+	</div>
+</div>
+<div class="grid grid-cols-2" id="about">
+	<div>
+		<p data-popup="popupMac" class="text-sm font-semibold">press space</p>
+		<canvas use:popup={popupMac} class="" id="canvas3d"></canvas>
+	</div>
+	<div class="flex justify-center items-center">
+		<div>
+			<h1 class="h1">About me</h1>
+			<p class="mt-2">
+				Hey there, my name is Neville Brem and I am a Web Developer from Zurich, Switzerland. <br />
+				I love coding, computer science and everything to do with tech. <br />
+				I am 16 years old and visit the Mathematical Scientific High School of Zurich.
+			</p>
+		</div>
 	</div>
 </div>
