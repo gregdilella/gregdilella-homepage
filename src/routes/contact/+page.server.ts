891918f860b0b3c3d7c7b56340b55aa587d1e537
Neville Brem <bremneville@gmail.com>
2024-01-12 07:10:44 +0100

    feat: began implementing contact form

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,16 +1,25 @@
 import type { Actions } from './$types';
-import emailjs from '@emailjs/browser';
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms/server';
+import { fail } from '@sveltejs/kit';
+
+const formSchema = z
+	.object({
+		name: z.string().min(3, 'Is that really your name?'),
+		email: z.string().email('Is this an email?'),
+		message: z.string().min(5, 'Your message is too short')
+	})
+	.required();
+
+export const load = async () => {
+	const form = await superValidate(formSchema);
+	return { form };
+};
 
 export const actions = {
-	default: async (event) => {
-		console.log('Hello');
-		emailjs.sendForm('service_m5dhn7u', 'template_0ea3ewq', event.target, '0tvqgP1sw-5fQmDto').then(
-			(result) => {
-				console.log('SUCCESS!', result.text);
-			},
-			(error) => {
-				console.log('FAILED...', error.text);
-			}
-		);
+	default: async ({ request }) => {
+		const form = await superValidate(request, formSchema);
+		if (!form.valid) return fail(400, { form });
+		return { form };
 	}
 } satisfies Actions;
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { superForm } from 'sveltekit-superforms/client';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const { form, enhance, errors } = superForm(data.form, {
+		resetForm: false
+	});
 </script>
 
-<main class="2xl:max-w-[70%] 2xl:mx-auto">
+<main class="2xl:max-w-[50%] 2xl:mx-auto">
 	<div class="text-center">
 		<div class="my-16">
 			<h1 class="font-heading-token font-black text-7xl">Contact</h1>
 			<h4 class="h4 font-semibold italic">Where to find me</h4>
 		</div>
 	</div>
-	<form use:enhance method="POST">
-		<input class="input" type="text" name="user_name" />
-		<input class="input" type="email" name="user_email" />
-		<input class="input" type="text" name="message" />
-		<input type="submit" value="Send" />
+	<form method="POST" use:enhance class="flex flex-col gap-4">
+		<div class="flex flex-col gap-2">
+			<label for="name">Name</label>
+			<input class="input" bind:value={$form.name} type="text" name="name" />
+			{#if $errors.name}
+				<span class="text-error-500">{$errors.name}</span>
+			{/if}
+		</div>
+		<div class="flex flex-col gap-2">
+			<label for="email">Email</label>
+			<input class="input" bind:value={$form.email} type="email" name="email" />
+			{#if $errors.email}
+				<span class="text-error-500">{$errors.email}</span>
+			{/if}
+		</div>
+		<div class="flex flex-col gap-2">
+			<label for="message">Message</label>
+			<input class="input" bind:value={$form.message} type="text" name="message" />
+			{#if $errors.message}
+				<span class="text-error-500">{$errors.message}</span>
+			{/if}
+		</div>
+		<button class="btn variant-filled-primary mx-auto">Submit!</button>
 	</form>
 </main>
