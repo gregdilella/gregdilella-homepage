e8434e76601a57614c5fc79222f2b1a53aa1c1b2
Neville Brem <bremneville@gmail.com>
2024-07-17 18:24:28 +0200

    added sizes property

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			images: {
-				formats: ['image/webp']
+				formats: ['image/webp'],
+				sizes: [640, 750, 828, 1080, 1200, 1920]
 			}
 		})
 	}
