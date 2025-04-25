168837d4d7bf27bef8a16aefda15bccdab9fc7f0
Gregdilella <gregdilella@gmail.com>
2025-04-24 20:28:34 -0400

    switch to adapter cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltekit/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
